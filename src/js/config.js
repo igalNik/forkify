8eb4ffbec1cd416c027f3c4f61d210f538b74f75
igalNik <igal.nikolaev@gmail.com>
2021-11-30 17:36:29 +0200

    Setting a new key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
-export const API_KEY = 'a96da2e1-a1d8-426e-bebe-f906ffd53614';
+export const API_KEY = '82cafcd4-4ede-426a-a73c-405e972bca80';
 export const TIMEOUT_SEC = 10;
 export const MODAL_CLOSE_SEC = 2.5;
 export const RESULTS_PER_PAGE = 10;
